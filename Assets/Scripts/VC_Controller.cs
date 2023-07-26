a59d086e808e98f02a635627f4b408a8adc851b2
ilpalee <pablo_ar2009@hotmail.es>
2023-07-26 06:06:28 -0300

    pasillo

---
+++ Assets/Scripts/VC_Controller.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class VC_Controller : MonoBehaviour
+{
+
+
+    public CinemachineVirtualCamera Vc_Lobby;
+    public CinemachineVirtualCamera Vc_Pasillo;
+    
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update() 
+    {
+        
+
+
+        
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
