9e92d97dd9d7fa9279d379217698f001e58d33d0
ilpalee <pablo_ar2009@hotmail.es>
2023-08-07 21:04:10 -0300

    Programando Transiciones de Cámara Main

--- Assets/Scripts/CamaraRefresh.cs
+++ Assets/Scripts/CamaraRefresh.cs
@@ -11,19 +11,15 @@ public class CamaraRefresh : MonoBehaviour
     public Vector3 playerChange;
 
     private CameraFollow cam;
+    private Vector2 originalCamMax;
+    private Vector2 originalCamMin;
     
     void Start()
     {
         cam = Camera.main.GetComponent<CameraFollow>();
-
+        originalCamMax = cam.MaxPos;
+        originalCamMin = cam.MinPos;
     }
-
-    
-    void Update()
-    {
-        
-    }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -33,13 +29,26 @@ public class CamaraRefresh : MonoBehaviour
         }
     }
 
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Personaje1")
+        {
+            RestaurarCamara();
+        }
+    }
+
     private void ActualizacionCamara(Collider2D other)
     {
-        cam.MaxPos += camMaxChange;
-        cam.MinPos += camMinChange;
+        cam.MaxPos = new Vector2(originalCamMax.x + camMaxChange.x, originalCamMax.y + camMaxChange.y);
+        cam.MinPos = new Vector2(originalCamMin.x + camMinChange.x, originalCamMin.y + camMinChange.y);
 
         other.transform.position += playerChange;
     }
 
+    private void RestaurarCamara()
+    {
+        cam.MaxPos = originalCamMax;
+        cam.MinPos = originalCamMin;
+    }
 
 }
