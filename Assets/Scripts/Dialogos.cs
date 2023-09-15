a43079500221a566e6885bbacaa217f142ec7813
ilpalee <pablo_ar2009@hotmail.es>
2023-09-15 00:57:05 -0300

    Nuevo sistema de dialogos

--- Assets/Scripts/Dialogos.cs
+++ Assets/Scripts/Dialogos.cs
@@ -2,92 +2,128 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Dialogos : MonoBehaviour
 {
-    public GameObject PanelDialogo;
-    public TextMeshProUGUI TextoDialogo;
-    public string[] Dialogo;
-    private int index;
-    public float VelocidadTexto;
 
-    public float Espaciado;
-    public bool EnRango;
-    
+    [SerializeField] private GameObject PanelDialogo;
+    [SerializeField] private TMP_Text TextoDialogo;
+    [SerializeField, TextArea(4, 6)] private string [] LineasDialogo;
+
+    [SerializeField] private Sprite imagenDialogo;
+    [SerializeField] private string NombreSpeaker;
+
+    private bool EnRango;
+    private bool Dialogando;
+    private int lineIndex;
+    public float TiempoTipeo; 
 
 
-    // Update is called once per frame
+    void Start()
+    {
+       Canvas canvas = FindObjectOfType<Canvas>();
+        if (canvas != null)
+        {
+            PanelDialogo = canvas.transform.Find("PanelDialogo").gameObject;
+            TextoDialogo = PanelDialogo.transform.Find("TextoDialogo").GetComponent<TMP_Text>();
+        }
+    }
+
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Z) && EnRango)
+        if (EnRango && Input.GetKeyDown(KeyCode.Space))
         {
-            if (PanelDialogo.activeInHierarchy)
+            if(!Dialogando)
             {
-                LimpiarTexto();
+                IniciarDialogo();
             }
-            else
+            else if (TextoDialogo.text == LineasDialogo[lineIndex])
             {
-                PanelDialogo.SetActive(true);
-                StartCoroutine(Typing());
+                SiguienteLinea();
+            }
+        }
+    }
+
+    private void IniciarDialogo()
+    {
+        Dialogando = true;
+        PanelDialogo.SetActive(true);
+        lineIndex = 0;
+        Time.timeScale = 0;
+
+        Transform childText = PanelDialogo.transform.Find("NombreSpeaker");
+
+        if (childText != null)
+        {
+            
+            TMP_Text textoComponent = childText.GetComponent<TMP_Text>();
+
+            if (textoComponent != null)
+            {
+                
+                textoComponent.text = NombreSpeaker;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && EnRango)
+
+        Transform childImage = PanelDialogo.transform.Find("Retrato");
+
+        if (childImage != null)
         {
-            SiguienteLinea();
+        
+        Image imageComponent = childImage.GetComponent<Image>();
+        
+        if (imageComponent != null)
+        {
+            
+            imageComponent.sprite = imagenDialogo;
         }
-    }
-
-
-    public void LimpiarTexto()
-    {
-        TextoDialogo.text = "";
-        index = 0;
-        PanelDialogo.SetActive(false);
-    }
-
-
-    IEnumerator Typing()
-    {
-        foreach (char letter in Dialogo[index].ToCharArray())
-
-        {
-            TextoDialogo.text += letter;
-            yield return new WaitForSeconds(VelocidadTexto);
         }
+        StartCoroutine(ShowLine());
     }
 
-
-    public void SiguienteLinea()
-
+    private void SiguienteLinea()
     {
-        if (index < Dialogo.Length - 1)
+        lineIndex++;
+        if(lineIndex < LineasDialogo.Length)
         {
-            index++;
-            TextoDialogo.text = "";
-            StartCoroutine(Typing());
+            StartCoroutine(ShowLine());
         }
         else
         {
-            LimpiarTexto();
+            Dialogando = false;
+            PanelDialogo.SetActive(false);
+            Time.timeScale = 1;
         }
     }
 
-
-    private void OnTriggerEnter2D(Collider2D other)
+    private IEnumerator ShowLine()
     {
-        if (other.CompareTag("Personaje1"))
+        TextoDialogo.text = string.Empty;
+        
+        foreach(char ch in LineasDialogo [lineIndex])
+        {
+            TextoDialogo.text += ch;
+            yield return new WaitForSecondsRealtime(TiempoTipeo);
+        } 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Personaje1"))
         {
             EnRango = true;
         }
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (other.CompareTag("Personaje1"))
+        if(collision.gameObject.CompareTag("Personaje1"))
         {
             EnRango = false;
-            LimpiarTexto();
         }
     }
+
 }
